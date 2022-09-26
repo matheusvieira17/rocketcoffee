e45ff4e6a8cf56508657dc41848df5d5a80bc632
Matheus Vieira Ferreira <matheusvferreira17@gmail.com>
2022-09-26 09:56:56 -0300

    The mobile menu has been added

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let open = true
+
 const navbar = document.querySelector(".navbar")
 const closeNavbar = document.querySelector(".burguer-btn")
 const menuMobile = document.querySelector(".menu-mobile")
 
-menuMobile.addEventListener("click", () => {
+const menuHamburguer = () => {
     closeNavbar.classList.toggle("close", open)
     navbar.classList.toggle("open", open)
     open = !open
-})
+}
+menuMobile.addEventListener("click", menuHamburguer)
